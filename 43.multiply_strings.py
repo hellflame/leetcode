1f613e9f219eeb8cd1a9b40111fa8288940d1719
hellflame <hellflamedly@gmail.com>
2017-08-08 09:35:21 +0800

    multiply string

---
+++ 43.multiply_strings.py
@@ -0,0 +1,36 @@
+# coding=utf8
+# https://leetcode.com/problems/multiply-strings/description/
+# Medium
+
+import numpy
+import re
+
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        result = numpy.convolve([int(x) for x in num1], [int(x) for x in num2])
+        index = len(result) - 1
+        carry = 0
+        while index >= 0:
+            result[index] += carry
+            carry = result[index] / 10
+            result[index] %= 10
+            index -= 1
+        if carry:
+            result = str(carry) + ''.join([str(x) for x in result])
+        else:
+            result = ''.join([str(x) for x in result])
+        
+        if not result.startswith("0"):
+            return result
+        else:
+            regs = re.compile("0{1,}(\d*)")
+            tmp = regs.findall(result)[0]
+            if not tmp:
+                return "0"
+            return tmp
+
