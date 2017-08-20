05767a3caaa17625db9d16da70356d31250ef33c
hellflame <hellflamedly@gmail.com>
2017-08-20 14:57:34 +0800

    update

--- 326.power_of_three.py
+++ 326.power_of_three.py
@@ -10,6 +10,7 @@ class Solution(object):
         :rtype: bool
         """
         # 由于测试用例使用了32位int，所以大多数人都是这么作弊的=。=
+        # 然而实际上在python中到了 3 ** 39 依然还是int
         return n > 0 and (1162261467 % n == 0)
 
 
