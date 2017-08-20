50dd1aeec62908a1b875761804d241c38925bdfd
hellflame <hellflamedly@gmail.com>
2017-08-20 14:34:06 +0800

    intersection of two arrays

---
+++ 349.intersection_of_two_arrays.py
@@ -0,0 +1,14 @@
+# coding=utf8
+# https://leetcode.com/problems/intersection-of-two-arrays/description/
+# Easy
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1) & set(nums2))
+
+
